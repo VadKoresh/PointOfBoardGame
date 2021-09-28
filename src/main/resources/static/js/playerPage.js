3621f1ceb8179e6949460504e360ccbc6a392eae
MSLadanov <86907817+MSLadanov@users.noreply.github.com>
2021-09-28 22:39:58 +0300

    Сделана верстка личного кабинета, добавлены новые классы в CSS, добавлено изображение фона для личного кабинета и изменен тайтл турнирной таблицы

---
+++ src/main/resources/static/js/playerPage.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    $('#changeInfo').click(function () {
+        $('#changeInfoForm').modal('show');
+    });
+
+    $(document).ready(function () {
+
+
+    });
+});
