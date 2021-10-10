a504f6a9d9f6bb5ed31c638cfd312c22e48a3ca1
MSLadanov <86907817+MSLadanov@users.noreply.github.com>
2021-10-10 12:13:44 +0300

    Сделана история побед

--- src/main/resources/static/js/playerPage.js
+++ src/main/resources/static/js/playerPage.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentLoc = String(window.location);
     let newLoc = currentLoc.substring(currentLoc.lastIndexOf('/') + 1);
 
-    fetch('https://pointofboardgames.herokuapp.com/story/${newLoc}')
+    fetch('https://pointofboardgames.herokuapp.com/story/' + newLoc)
         .then(response => response.json())
         .then(data => data.forEach(item => {
         let card = document.createElement('tr');
